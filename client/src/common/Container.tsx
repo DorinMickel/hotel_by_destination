1afecf7424d331a9e9661a01d0816547bbcfc235
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2022-02-01 20:40:20 +0700

    clean unnecessary code and files

--- client/src/common/Container.tsx
+++ client/src/common/Container.tsx
@@ -1,41 +1,18 @@
 
 import styled from "styled-components";
 
-const Container = styled.div<{isRow?: boolean}>`
+const Container = styled.div<{isRow?: boolean, justifyContent?: string, isNotAlign?: boolean}>`
     display: flex;
     max-width: 1320px;
-    justify-content: center;
-    align-items: center;
+    justify-content: ${({justifyContent}) => justifyContent ? justifyContent : 'center'};
+    align-items: ${({isNotAlign}) => isNotAlign ? null : 'center'};
     margin: 8px;
     width: 100%;
     margin-right: auto;
     margin-left: auto;
     flex-direction: ${({isRow}) => isRow ? 'row' : 'column'};
-
     `
 
-// const StyledContainer = styled.div<{theme: DefaultTheme, isRow: boolean}>`
-//     display: flex;
-//     max-width: 1320px;
-//     justify-content: center;
-//     align-items: center;
-//     margin: 8px;
-//     width: 100%;
-//     margin-right: auto;
-//     margin-left: auto;
-//     flex-direction: ${({isRow}) => isRow ? 'row' : 'column'};
-// `
-// interface ContainerProps {
-//     isRow?: boolean;
-//     theme?: DefaultTheme;
-// }
 
-
-
-// const Container: React.FC<ContainerProps> = (props, {isRow}) => {
-//     return (
-//         <StyledContainer isRow={isRow}>{props.children}</StyledContainer>
-//     )
-// }
 
 export default Container
--- client/src/common/ImgDiv.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const ImgDiv = styled.div`
-    padding: 5px;
-    width: 30%
-`
-
-export default ImgDiv
